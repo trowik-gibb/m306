c9a253d9a9d0444a0e7a67237c1f6966902b6bb8
carry <carrycharge@gmail.com>
2020-10-19 14:36:38 +0200

    angular setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { StartPageComponent } from './start-page/start-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageThreeComponent } from './page-three/page-three.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: StartPageComponent },
+  { path: '1', component: PageOneComponent },
+  { path: '2', component: PageTwoComponent},
+  { path: '3', component: PageThreeComponent},
+  { path: '**', component: PageOneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
