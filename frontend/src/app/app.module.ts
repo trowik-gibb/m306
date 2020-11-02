26184fafd0da720c1ae92fd8eba8b7a801c5c246
carry <carrycharge@gmail.com>
2020-11-02 14:55:17 +0100

    modal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,35 +1,38 @@
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './start-page/start-page.component';
 import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { PageOneComponent } from './page-one/page-one.component';
 import { PageTwoComponent } from './page-two/page-two.component';
 import { PageThreeComponent } from './page-three/page-three.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { LoginComponent } from './login/login.component';
+import { CreateModalComponent } from './create-modal/create-modal.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     StartPageComponent,
-    PageOneComponent,
+    HomeComponent,
     PageTwoComponent,
     PageThreeComponent,
-    LoginComponent
+    LoginComponent,
+    CreateModalComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbPaginationModule, 
-    NgbAlertModule,
-    NgbModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbPaginationModule,
+        NgbAlertModule,
+        NgbModule,
+        FormsModule
 
-  ],
+    ],
   providers: [
     AuthService,
     AuthGuardService
